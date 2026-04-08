595a85a45a00d9e55e89466ccf375c8aaa50119b
gabrielwelsing <gabriel.welsing@gmail.com>
2026-04-08 18:56:41 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'https://www.sup-ia.com',
     'https://www.numposte.sup-ia.com',
+    'https://www.hub.sup-ia.com',
     /\.railway\.app$/
 ];
 app.use(cors({
